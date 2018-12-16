d74cee7c9a976b1b7a28c845b9851b3b850277ae
Divyanshu Gupta <divyanshu30gupta@gmail.com>
2018-12-16 13:05:05 +0530

    minor bug fixes

--- CIFAR-10 Training/cifar_data_prep.m
+++ CIFAR-10 Training/cifar_data_prep.m
@@ -5,7 +5,7 @@ fprintf('Preparing Data into Trainable format. \n');
 it = 1;
 for b = 1:5
     for i = 1:10000
-        Xtrain{it} = im2double(uint8(squashR(X{1,b}.data(i,:),zeros(32,32,3))));
+        Xtrain{it} = normalize3(rgb2gray(im2double(uint8(squashR(X{1,b}.data(i,:),zeros(32,32,3))))));
         labels(it,1) = double(y{1,1}.labels(i,1));
         ytrain(it,:) = yGen(labels(it,1) + 1,10);
         it = it + 1;
